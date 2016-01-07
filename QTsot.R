eb0e547402d3a1f9faada879ff3acf7caf27d7af
cm2125 <cm2125@columbia.edu>
2016-01-07 15:48:18 -0500

    cm2125 change
    Added something to line 5

--- QTsot.R
+++ QTsot.R
@@ -2,7 +2,7 @@ getwd()
 
 ## test edit MAC Jan5th 1
 ## test edit MAC Jan7th 1
-
+##Sudy edit Jan 7 2016
 setwd("C:/Users/Sudy/Desktop/QT2/")
 qtsot <- read.csv("RF_Coded_11.10.csv", header=TRUE)
 options(digits=3)
