3b84d4ff3bad148821fb3416612a1b2357f500f0
mac2393 <mac2393@columbia.edu>
2016-01-19 12:06:35 -0500

    Jan 19 Mark 1

--- QTsot.R
+++ QTsot.R
@@ -2,8 +2,8 @@ getwd()
 
 ## test edit MAC Jan5th 1
 ## test edit MAC Jan7th 1
-##Sudy edit Jan 7 2016
-# Hi Sudy
+## Sudy edit Jan 7 2016
+#  Hi Sudy
 
 setwd("C:/Users/Sudy/Desktop/QT2/")
 qtsot <- read.csv("RF_Coded_11.10.csv", header=TRUE)
