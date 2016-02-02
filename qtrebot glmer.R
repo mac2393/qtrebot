197c9b5d92ec303a2aeddc07e48b6d0b1f696815
mac2393 <mac2393@columbia.edu>
2016-02-02 15:42:54 -0500

    February 2nd

--- qtrebot glmer.R
+++ qtrebot glmer.R
@@ -10,6 +10,9 @@ plot(allEffects(glmer1))
 
 glmer1.0 <- glmer(dec ~ avgbot + path + ( product | subj), data=qtsot, family=binomial)
 summary(glmer1.0)  # this model does not fail to converge
+plot(allEffects(glmer1.0))
+
+glmer
 
 glmer1.1 <- glmer(dec ~ avgbot + product + (product|subj), data=qtsot, family=binomial)
 summary(glmer1.1)
@@ -19,9 +22,13 @@ glmer1.1i <- glmer(dec ~ avgbot * product + (product|subj), data=qtsot, family=b
 summary(glmer1.1i) # Model failed to converge with max|grad| = 0.0967525 (tol = 0.001, component 1) # Model is nearly unidentifiable: very large eigenvalue # Rescale variables?
 plot(allEffects(glmer1.1i))
 
-glmer1.2 <- glmer(dec ~ avgbot + path + (product|subj), data=qtsot, family=binomial)
+glmer1.2 <- glmer(dec ~ avgbot * path + (product|subj), data=qtsot, family=binomial)
 summary(glmer1.2)
 plot(allEffects(glmer1.2))
+
+glmer1.2a <- glmer(dec ~ path * re + (product|subj), data=qtsot, family=binomial)
+summary(glmer1.2a)
+plot(allEffects(glmer1.2a))
 
 glmer1.2i <- glmer(dec ~ avgbot * path + (product|subj), data=qtsot, family=binomial)
 summary(glmer1.2i)
@@ -31,6 +38,18 @@ glmer1.3 <- glmer(dec ~ avgbot + re + (product|subj), data=qtsot, family=binomia
 summary(glmer1.3)
 plot(allEffects(glmer1.3)) # no interaction 
 
+glmer3.1 <- glmer(dec ~ path + re + (product|subj), data=qtsot, family=binomial)
+summary(glmer3.1)
+plot(allEffects(glmer3.1)
+
+glmer3.1i <- glmer(dec ~ path * req + (product|subj), data=qtsot, family=binomial)
+summary(glmer3.1i)
+plot(allEffects(glmer3.1i)     
+
+glmer3.2i <- glmer(dec ~ avgbot * req + (product|subj), data=qtsot, family=binomial)
+summary(glmer3.2i)
+plot(allEffects(glmer3.2i)      
+     
 glmer1.3i <- glmer(dec ~ avgbot * re + (product|subj), data=qtsot, family=binomial)
 summary(glmer1.3i)
 
@@ -38,7 +57,7 @@ glmer1.4 <- glmer(dec ~ avgbot + gender + (product|subj), data=qtsot, family=bin
 summary(glmer1.4) # fail to converge
 
 glmer1.5 <- glmer(dec ~ req + avgbot + (product|subj), data=qtsot, family=binomial)
-summary(glmer1.5)
+summary(glmer1.5) #  fails to converge
 
 glmer2 <- glmer(dec ~ gender * req + ( subj | product), data=qtsot, family=binomial)
 summary(glmer2)
